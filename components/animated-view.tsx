51ce6d481606b87e2d4917de0a75180157debc00
Diego <diegofreeman78@gmail.com>
2025-11-23 07:47:13 +0100

    feat(animated-view): add animation state and background transition
    Implement smooth background color transition and animation state management using react-native-reanimated. Add timing controls for animation completion and background change effects.

--- components/animated-view.tsx
+++ components/animated-view.tsx
@@ -1,18 +1,49 @@
+import { useAnimationStore } from '@/hooks/use-animation';
 import LottieView from 'lottie-react-native';
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { StyleSheet } from 'react-native-unistyles';
 
 export const AnimatedView = () => {
-  const onNavigate = () => {};
+  const setIsFinished = useAnimationStore((state) => state.setIsFinished);
+  const backgroundColor = useSharedValue<'#fff' | '#000000'>('#fff');
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColor.value,
+  }));
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      backgroundColor.value = withTiming('#000000', { duration: 600 });
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, [backgroundColor]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsFinished(true);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [setIsFinished]);
+
   return (
-    <View style={{ flex: 1 }}>
+    <Animated.View style={[styles.container, animatedStyle]}>
       <LottieView
         source={require('@/assets/images/animation.json')}
+        // ref={animationRef}
         autoPlay
-        loop
-        onAnimationFinish={onNavigate}
-        style={{ flex: 1 }}
+        style={styles.container}
       />
-    </View>
+    </Animated.View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
