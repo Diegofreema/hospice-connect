44638ac90dcd27341f63da952884543834f76216
Diego <diegofreeman78@gmail.com>
2026-03-27 16:25:20 +0100

    feat: Configure RevenueCat SDK with the authenticated user's ID and conditionally run the effect.

--- hooks/rc/use-configure-rc.ts
+++ hooks/rc/use-configure-rc.ts
@@ -1,3 +1,4 @@
+import { useAuth } from '@/components/context/auth';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import Purchases, { LOG_LEVEL } from 'react-native-purchases';
@@ -5,13 +6,21 @@ import Purchases, { LOG_LEVEL } from 'react-native-purchases';
 //...
 
 export const useConfigureRC = () => {
+  const { user } = useAuth();
   useEffect(() => {
+    if (!user?.id) return;
     Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
 
     if (Platform.OS === 'ios') {
-      Purchases.configure({ apiKey: 'appl_iNVtzSygqQqsuOTTEhwumMBAgow' });
+      Purchases.configure({
+        apiKey: 'appl_iNVtzSygqQqsuOTTEhwumMBAgow',
+        appUserID: user?.id,
+      });
     } else if (Platform.OS === 'android') {
-      Purchases.configure({ apiKey: 'goog_nOEvpbJXIvSITLBWyRHueuAnPBD' });
+      Purchases.configure({
+        apiKey: 'goog_nOEvpbJXIvSITLBWyRHueuAnPBD',
+        appUserID: user?.id,
+      });
     }
-  }, []);
+  }, [user?.id]);
 };
